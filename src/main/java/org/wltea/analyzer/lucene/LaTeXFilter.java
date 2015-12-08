87a7d42e03afdcd560baa0c072104935fe2b2567
two8g <ybxnice@gmail.com>
2015-12-08 09:44:31 +0800

    fix LaTeXFilter bug buffer已经改变

--- src/main/java/org/wltea/analyzer/lucene/LaTeXFilter.java
+++ src/main/java/org/wltea/analyzer/lucene/LaTeXFilter.java
@@ -10,28 +10,29 @@ import java.io.IOException;
 /**
  * Created by 易斌鑫 on 15-12-7.
  */
-public class LaTeXFilter extends TokenFilter{
-    private final TypeAttribute typeAtt = (TypeAttribute)this.addAttribute(TypeAttribute.class);
-    private final CharTermAttribute termAtt = (CharTermAttribute)this.addAttribute(CharTermAttribute.class);
+public class LaTeXFilter extends TokenFilter {
+    private final TypeAttribute typeAtt = (TypeAttribute) this.addAttribute(TypeAttribute.class);
+    private final CharTermAttribute termAtt = (CharTermAttribute) this.addAttribute(CharTermAttribute.class);
+
     protected LaTeXFilter(TokenStream input) {
         super(input);
     }
 
     @Override
     public boolean incrementToken() throws IOException {
-        if(!this.input.incrementToken()){
+        if (!this.input.incrementToken()) {
             return false;
-        }else {
+        } else {
             char[] buffer = this.termAtt.buffer();
             int bufferLength = this.termAtt.length();
             String type = this.typeAtt.type();
-            if(type.startsWith("TYPE_LATEX")){
+            if (type.startsWith("TYPE_LATEX")) {
                 int upto = 0;
-                for(int i = 0; i < bufferLength; ++i) {
+                for (int i = 0; i < bufferLength; ++i) {
                     char c = buffer[i];
-                    if(c != 32 && c!=123 && c!=125 ) {
+                    if (c != 32 && c != 123 && c != 125) {
                         buffer[upto++] = c;
-                    }else if(c==125 && buffer[i-1]==123){
+                    } else if (i + 1 < bufferLength && c == 123 && buffer[i + 1] == 125) {
                         //用空格代替}{
                         buffer[upto++] = 32;
                     }
